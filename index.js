c5e0538571fcdc2d3e67a8ceaab4c2f3cbce57fe
Alex Davis <davis.alex.jr@gmail.com>
2015-09-30 13:46:03 -0400

    Added request to get apartment array. Added ability to show some data from the array.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-    // YOUR CODE HERE!
+    $.ajax({
+    	url: "http://api.myjson.com/bins/2sadq?pretty=1",
+    	dataType: "json",
+    	success: function(response){
+    		$.each(response.apartments, function(i, apartment){
+			  var listing = "<a href='#' class='list-group-item'><h4 class='list-group-item-heading'>" + apartment.description  + "/ " + apartment.bedrooms + " / " + apartment.price + " </h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
+
+			  $(".apartments").append(listing);
+    		});
+    	}
+    });
 });
 
 
